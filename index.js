dc8e6297cfc9d4b0c13503a1ae51eb055486ca0a
Felicitas Montero <felimontero2016@gmail.com>
2022-11-11 16:14:25 -0300

    Segunda Pre-Entrega

--- index.js
+++ index.js
@@ -8,34 +8,66 @@ let usuario = prompt('Ingresa tu nombre')
 presentacionUsuario(usuario)
 
 
-let cabaña = parseInt(prompt('Escoge la cabaña que quieras reservar: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'))
-
+let cabañaElegida = parseInt(prompt('Escoge la cabaña que quieras reservar: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'))
 
 let totalEstadia = 0
 let seguirViendo = true
 let decision
 
+//array
+const cabañasDisponibles = []
+
+class Cabaña {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const Ceibos = new Cabaña(1, 12500)
+cabañasDisponibles.push(Ceibos)
+const Rosedal = new Cabaña(2, 6900)
+cabañasDisponibles.push(Rosedal)
+const Lavandas = new Cabaña(3, 9000)
+cabañasDisponibles.push(Lavandas)
+const violetas = new Cabaña(4, 8500)
+cabañasDisponibles.push(violetas)
+const jazmines = new Cabaña(5, 5990)
+cabañasDisponibles.push(jazmines)
+
+
 while (seguirViendo === true) {
-    if (cabaña === 1) {
-        totalEstadia = totalEstadia + 12500
-    } else if (cabaña === 2) {
-        totalEstadia = totalEstadia + 6900
-    } else if (cabaña === 3) {
-        totalEstadia = totalEstadia + 9000
-    } else if (cabaña === 4) {
-        totalEstadia = totalEstadia + 8500
-    } else if (cabaña === 5) {
-        totalEstadia = totalEstadia + 5990
+
+    const cabañaCliente = cabañasDisponibles.find(cabaña => cabaña.nombre === cabañaElegida)
+    if (cabañaCliente) {
+        totalEstadia = totalEstadia + cabañaCliente.precio
     } else {
-        cabaña = parseInt(prompt('Escoge una cabaña disponible: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'))
+        cabañaElegida = parseInt(prompt('Escoge una cabaña disponible: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'))
         continue
     }
+    console.log(cabañaCliente)
 
+
+    
+    // if (cabañaElegida === 1) {
+    //     totalEstadia = totalEstadia + Ceibos.precio
+    // } else if (cabañaElegida === 2) {
+    //     totalEstadia = totalEstadia + Rosedal.precio
+    // } else if (cabañaElegida === 3) {
+    //     totalEstadia = totalEstadia + Lavandas.precio
+    // } else if (cabañaElegida === 4) {
+    //     totalEstadia = totalEstadia + violetas.precio
+    // } else if (cabañaElegida === 5) {
+    //     totalEstadia = totalEstadia + jazmines.precio
+    // } else {
+    //     cabaña = parseInt(prompt('Escoge una cabaña disponible: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'))
+    //     continue
+    // }
 
 
     decision = parseInt(prompt('Quieres seguir comprando? 1.Si - 2.No'))
     if (decision === 1) {
-        cabaña = parseInt(
+        cabañaElegida = parseInt(
             prompt(
                 'Escoge una cabaña disponible: 1.Los Ceibos - 2.El Rosedal - 3.Las Lavandas - 4. Las violetas - 5.Los jazmines'
             )
@@ -50,7 +82,6 @@ alert(`El total de tu estadia es ${totalEstadia}`)
 
 function diasEstadia(dias) {
 
-
     if (dias === 1) {
         alert(`Tu estadia en la cabaña durara tres dias`)
     } else if (dias === 2) {
@@ -64,3 +95,5 @@ function diasEstadia(dias) {
 let dias = parseInt(prompt('Ingresa la cantidad de dias: 1.Tres dias- 2.Siete dias - 3.Diez dias'))
 
 diasEstadia(dias)
+
+
